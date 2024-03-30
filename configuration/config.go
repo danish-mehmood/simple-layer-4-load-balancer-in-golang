c78fa06554f435ff1935e5bfaa5c7ca59c5009c4
danish@jedi <danish.mehmood@attech-ltd.com>
2024-03-30 22:40:34 +0500

    done with the config

---
+++ configuration/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+type Configuration struct{
+	HealthCheckInterval string   `json:"healthCheckInterval"`
+	Servers             []string `json:"servers"`
+	ListenPort          string   `json:"address"`
+	Algorithm           string   `json:"algorithm"`
+}
+
+func (c *Configuration) CreateNewConfig(file string)(error){
+	
+    bytes , err := ioutil.ReadFile(file)
+	fmt.Println("the file bytes" , string(bytes) , )
+	if err !=nil{
+		fmt.Println(err)
+		return  err
+	}
+	
+    if err = json.Unmarshal(bytes , c);err!=nil{
+		fmt.Println(err)
+		return  err
+	}
+
+   
+    return nil
+	}
--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	configuration "github.com/danish-mehmood/simple-layer-4-load-balancer-in-golang/configuration"
+)
 func main (){
-	fmt.Println("done")
+	c:=  configuration.Configuration{}
+	c.CreateNewConfig("config.json")
+	fmt.Println("done" , c)
 }
